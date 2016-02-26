30c63879d567613d8f49c96bfe2f471e51eee662
sziolkow <sziolkow@gmail.com>
2016-02-26 11:54:38 +0100

    Initial commit, simple eureka server

---
+++ simple-eureka-server/src/main/java/org/home/sziolkow/microservice/eurekaserver/ConfigurationServer.java
@@ -0,0 +1,15 @@
+package org.home.sziolkow.microservice.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServer.class, args);
+    }
+}
